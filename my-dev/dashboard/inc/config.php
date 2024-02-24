ec85356b680c9b1fbf2efa394119a07b699a3f69
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-02-24 01:19:40 -0400

    .

--- my-dev/dashboard/inc/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$_CONFIG['sitename'] = 'Test for now';
-$_CONFIG['domain'] = 'http://localhost';
-$_CONFIG['subfolder'] = 'dashboard';
-$_CONFIG['url'] = $_CONFIG['domain'] . '/' . $_CONFIG['subfolder'];
-
-?>
--- my-dev/logout.php
+++
@@ -1,3 +0,0 @@
-<?php
-  header('location: inc/logout.php');
-?>
