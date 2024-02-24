e7c7ecf74057a8754a8a3229358f76fe2466f8bb
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-02-24 01:14:13 -0400

    Removed useless files

--- khaos backup/inc/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set('America/Halifax');
-$_CONFIG['sitename'] = "Khaos Development";
-$_CONFIG['domain'] = "http://khaosdevelopment.com";
-
-$_CONFIG['dbhost'] = 'localhost';
-$_CONFIG['dbuser'] = 'root';
-$_CONFIG['dbpass'] = '';
-$_CONFIG['dbname'] = 'khaosdev';
