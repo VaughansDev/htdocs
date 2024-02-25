3c3b4715ea381892f72a95d33d49ac7eb8bee81e
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-02-25 18:12:40 -0400

    /

--- account/my-account.php
+++ account/my-account.php
@@ -25,102 +25,54 @@ if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] != TRUE) { ?>
     </header>
     <div class="kd-box-5 bg-grey-6">
         <div class="container">
-            <div class="row align-items-center justify-content-between vertical-gap mnt-30 sm-gap mb-50">
-                <div class="col-auto">
-                    <h2 class="h4 mb-0 mt-0">Your Tickets</h2>
+            <div class="row justify-content-center vertical-gap">
+                <div class="col-12 col-md-4 col-lg-3 kd-feature-variable">
+                    <div class="kd-feature kd-feature-3 kd-feature-color-1 kd-block-decorated">
+                        <div class="kd-feature-icon">
+                            <i class="fa-solid fa-file-lines"></i>
+                        </div>
+                        <div class="kd-feature-cont">
+                            <div class="kd-feature-title"><a href="documentations.php">Documentations</a></div>
+                            <div class="kd-feature-text">Phasellus interdum est diam, eu varius odio tincidunt eget ultrices et</div>
+                            <a href="documentations.php" class="kd-btn kd-btn-link d-flex kd-feature-link">Read More <i class="fa-solid fa-angle-right"></i></span></a>
+                        </div>
+                    </div>
                 </div>
-                <div class="col pl-30 pr-30 d-none d-sm-block">
-                    <div class="kd-separator ml-10 mr-10"></div>
+                <div class="col-12 col-md-4 col-lg-3 kd-feature-variable">
+                    <div class="kd-feature kd-feature-3 kd-feature-color-2 kd-block-decorated">
+                        <div class="kd-feature-icon">
+                            <i class="fa-solid fa-book-bookmark"></i>
+                        </div>
+                        <div class="kd-feature-cont">
+                            <div class="kd-feature-title"><a href="articles.php">Knowledge Base</a></div>
+                            <div class="kd-feature-text">Aliquam id nisi sit amet massa mollis lobortis interdum felis integer at arcu</div>
+                            <a href="articles.php" class="kd-btn kd-btn-link d-flex kd-feature-link">Read More <i class="fa-solid fa-angle-right"></i></span></a>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-auto">
-                    <a href="ticket-submit.php" class="kd-btn kd-btn-md">Submit a ticket</a>
+                <div class="col-12 col-md-4 col-lg-3 kd-feature-variable">
+                    <div class="kd-feature kd-feature-3 kd-feature-color-3 kd-block-decorated">
+                        <div class="kd-feature-icon">
+                            <i class="fa-solid fa-comments"></i>
+                        </div>
+                        <div class="kd-feature-cont">
+                            <div class="kd-feature-title"><a href="forum.php">Forums</a></div>
+                            <div class="kd-feature-text">Sed eget nibh justo euismod maximus et vel nisl pellentesque suscipit nisi</div>
+                            <a href="forum.php" class="kd-btn kd-btn-link d-flex kd-feature-link">Read More <i class="fa-solid fa-angle-right"></i></span></a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <a href="single-ticket.php" class="kd-ticket-item kd-ticket-new kd-ticket-open kd-block-decorated">
-                <span class="kd-ticket-img">
-                    <img src="<?php echo $_CONFIG['accounturl']; ?>/assets/images/avatar-1.png" alt="">
-                </span>
-                <span class="kd-ticket-cont">
-                    <span class="kd-ticket-name"> John Leonard </span>
-                    <span class="kd-ticket-title h5"> Need help with customization. Some options are not appearing... </span>
-                    <ul class="kd-ticket-info">
-                        <li>Update: 5 Nov 2018</li>
-                        <li>Product: Quantial</li>
-                        <li>Comments: 2</li>
-                        <li class="kd-ticket-new">New</li>
-                    </ul>
-                </span>
-                <span class="kd-ticket-status"> Open </span>
-            </a>
-            <a href="single-ticket.php" class="kd-ticket-item kd-ticket-closed kd-block-decorated">
-                <span class="kd-ticket-img">
-                    <img src="<?php echo $_CONFIG['accounturl']; ?>/assets/images/avatar-default.svg" alt="">
-                </span>
-                <span class="kd-ticket-cont">
-                    <span class="kd-ticket-name"> Bruno Rice </span>
-                    <span class="kd-ticket-title h5"> Theme not updating in downloads </span>
-                    <ul class="kd-ticket-info">
-                        <li>Update: 4 Nov 2018</li>
-                        <li>Product: Sensific</li>
-                        <li>Comments: 11</li>
-                    </ul>
-                </span>
-                <span class="kd-ticket-status"> Closed </span>
-            </a>
-        </div>
-    </div>
-    <div class="kd-separator"></div>
-    <div class="kd-box-5 pb-100">
-        <div class="container mt-4 mnb-7">
-            <div class="row align-items-center justify-content-between vertical-gap mnt-30 sm-gap mb-50">
-                <h2 class="col-auto h4 mb-0 mt-0">Purchased Items</h2>
-                <div class="col pl-30 pr-30 d-none d-sm-block">
-                    <div class="kd-separator ml-10 mr-10"></div>
-                </div>
-                <div class="col-auto">
-                    <a href="account-licenses.php" class="kd-btn kd-btn-md">View Licenses</a>
-                </div>
-            </div>
-            <div class="row vertical-gap">
-                <div class="col-12 col-md-6 col-lg-4">
-                    <a href="product.php" class="kd-portfolio-item kd-portfolio-item-style-2 kd-block-decorated">
-                        <span class="kd-portfolio-item-img">
-                            <img src="<?php echo $_CONFIG['accounturl']; ?>/assets/images/product-1-sm.png" alt="">
-                        </span>
-                        <span class="kd-portfolio-item-title">Quantial – Moving Upon Signs Moveth Lesser</span>
-                    </a>
-                </div>
-                <div class="col-12 col-md-6 col-lg-4">
-                    <a href="product.php" class="kd-portfolio-item kd-portfolio-item-style-2 kd-block-decorated">
-                        <span class="kd-portfolio-item-img">
-                            <img src="<?php echo $_CONFIG['accounturl']; ?>/assets/images/product-2-sm.png" alt="">
-                        </span>
-                        <span class="kd-portfolio-item-title">Sensific – Saying Beast Lesser for in Fruitful</span>
-                    </a>
-                </div>
-                <div class="col-12 col-md-6 col-lg-4">
-                    <a href="product.php" class="kd-portfolio-item kd-portfolio-item-style-2 kd-block-decorated">
-                        <span class="kd-portfolio-item-img">
-                            <img src="<?php echo $_CONFIG['accounturl']; ?>/assets/images/product-3-sm.png" alt="">
-                        </span>
-                        <span class="kd-portfolio-item-title">Minist – Subdue Above for Signs Dry is Have Great</span>
-                    </a>
-                </div>
-                <div class="col-12 col-md-6 col-lg-4">
-                    <a href="product.php" class="kd-portfolio-item kd-portfolio-item-style-2 kd-block-decorated">
-                        <span class="kd-portfolio-item-img">
-                            <img src="<?php echo $_CONFIG['accounturl']; ?>/assets/images/product-4-sm.png" alt="">
-                        </span>
-                        <span class="kd-portfolio-item-title">Desty – She&#39;d Under Light Over Multiply Stars Winged</span>
-                    </a>
-                </div>
-                <div class="col-12 col-md-6 col-lg-4">
-                    <a href="product.php" class="kd-portfolio-item kd-portfolio-item-style-2 kd-block-decorated">
-                        <span class="kd-portfolio-item-img">
-                            <img src="<?php echo $_CONFIG['accounturl']; ?>/assets/images/product-5-sm.png" alt="">
-                        </span>
-                        <span class="kd-portfolio-item-title">Silies – Grass Living Fly Subdue After Fruitful</span>
-                    </a>
+                <div class="col-12 col-md-4 col-lg-3 kd-feature-variable">
+                    <div class="kd-feature kd-feature-3 kd-feature-color-4 kd-block-decorated">
+                        <div class="kd-feature-icon">
+                            <i class="fa-solid fa-ticket"></i>
+                        </div>
+                        <div class="kd-feature-cont">
+                            <div class="kd-feature-title"><a href="ticket.php">Ticket System</a></div>
+                            <div class="kd-feature-text">Vivamus sceleriue libero velit blandit, hendrerit nisl at,dapibus sollicitudin</div>
+                            <a href="ticket.php" class="kd-btn kd-btn-link d-flex kd-feature-link">Read More <i class="fa-solid fa-angle-right"></i></span></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
