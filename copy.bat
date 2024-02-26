b77711a7c073761708d4c32af61598a681ee5bcb
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-02-26 05:56:47 -0400

    Delete copy.bat

--- copy.bat
+++
@@ -1,8 +0,0 @@
-robocopy A:\xampp\htdocs\inc A:\xampp\htdocs\account\inc /e
-robocopy A:\xampp\htdocs\inc A:\xampp\htdocs\blog\inc /e
-robocopy A:\xampp\htdocs\inc A:\xampp\htdocs\docs\inc /e
-robocopy A:\xampp\htdocs\inc A:\xampp\htdocs\forums\inc /e
-robocopy A:\xampp\htdocs\inc A:\xampp\htdocs\knowledgebase\inc /e
-robocopy A:\xampp\htdocs\inc A:\xampp\htdocs\store\inc /e
-robocopy A:\xampp\htdocs\inc A:\xampp\htdocs\support\inc /e
-pause:
