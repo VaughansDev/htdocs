d1d6e6673b557b1189062130f1169aa1b3981ce5
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-02-26 00:37:20 -0400

    wsd

--- support/inc/viewlicense.php
+++ support/inc/viewlicense.php
@@ -44,12 +44,14 @@ if (isset($_GET['lid'])) {
                             <div class="kd-comment-date"><?php echo $row['license_type']; ?></div>
                         </div>
                         <div class="col-lg-8">
-                            <a href="#" class="kd-comment-file kd-comment-file-jpg">
-                                <span class="kd-comment-file-img"><img src="<?php echo $_CONFIG['domain']; ?>/assets/images/icon-zip.svg" alt="" width="36"></span>
-                                <span class="kd-comment-file-name">example-file.jpg</span>
-                                <span class="kd-comment-file-size">4.8 MB</span>
-                                <span class="kd-comment-file-icon"><span class="icon pe-7s-download"></span></span>
-                            </a>
+                            <form>
+                                <button href="#" class="kd-comment-file kd-comment-file-jpg">
+                                    <span class="kd-comment-file-img"><img src="<?php echo $_CONFIG['domain']; ?>/assets/images/icon-zip.svg" alt="" width="36"></span>
+                                    <span class="kd-comment-file-name">example-file.jpg</span>
+                                    <span class="kd-comment-file-size">4.8 MB</span>
+                                    <span class="kd-comment-file-icon"><span class="icon pe-7s-download"></span></span>
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
