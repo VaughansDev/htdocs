35efa4eeeb7fb1d1916240db3faa8c280cd3179b
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-02-26 04:28:53 -0400

    x

---
+++ blog/inc/mytickets.php
@@ -0,0 +1,186 @@
+<?php echo $_CONFIG['ticketsurl']; ?>/ticket.php?ticketno=<?php echo $row['ticket_no']; ?>
+
+
+
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/inc/dbcon.php';
+if (isset($_GET['status'])) {
+    $status = $_GET['status'];
+    $accountid = $_SESSION['accountid'];
+    $total_pages = $dbcon->query("SELECT * FROM invoices WHERE accountid = '$accountid' AND paymentstatus = '$status'")->num_rows;
+    $page = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1;
+    $num_results_on_page = 12;
+    if ($stmt = $dbcon->prepare("SELECT * FROM invoices WHERE accountid = ? AND paymentstatus = ? LIMIT ?,?")) {
+        $calc_page = ($page - 1) * $num_results_on_page;
+        $stmt->bind_param('ssii', $accountid, $status, $calc_page, $num_results_on_page);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        while ($row = $result->fetch_assoc()) { ?>
+            <a href="<?php echo $_CONFIG['ticketsurl']; ?>/ticket.php?ticketno=<?php echo $row['ticket_no']; ?>" class="kd-ticket-item kd-ticket-new kd-ticket-open kd-block-decorated">
+                <span class="kd-ticket-img">
+                    <img src="<?php echo $_CONFIG['supporturl']; ?>/assets/images/avatar-1.png" alt="">
+                </span>
+                <span class="kd-ticket-cont">
+                    <span class="kd-ticket-name"> <?php echo $row['lastreplyname']; ?> </span>
+                    <span class="kd-ticket-title h5"> <?php echo $row['lastreplymsg']; ?> </span>
+                    <ul class="kd-ticket-info">
+                        <li>Update: <?php echo $row['lastreplydate'] . ' at ' . $row['lastreplyname']; ?>; ?></li>
+                        <li>Product: <?php echo $row['ticket_product']; ?></li>
+                        <li>Comments: <?php echo $row['ticketreplys']; ?></li>
+                        <?php if ($row['lastreplyaccountid'] != $_SESSION['accountid']) { ?>
+                            <li class="kd-ticket-new">New Reply</li>
+                        <?php } ?>
+                    </ul>
+                </span>
+                <span class="kd-ticket-status">
+                    <?php
+                    if ($row['paymentstatus'] == 'PAID') {
+                        echo '<div style="color: green;">PAID</div>';
+                    } elseif ($row['paymentstatus'] == 'DUE') {
+                        echo '<div style="color: red;">DUE</div>';
+                    } elseif ($row['paymentstatus'] == 'UNPAID') {
+                        echo '<div style="color: orange;">UNPAID</div>';
+                    } else {
+                        echo '<div style="color: blue;">UNKNOWN</div>';
+                    }
+                    ?>
+                </span>
+            </a>
+        <?php } ?>
+    <?php } else { ?>
+        <a href="<?php echo $_CONFIG['ticketsurl']; ?>/new-ticket.php" class="kd-ticket-item kd-ticket-new kd-ticket-open kd-block-decorated">
+            <span class="kd-ticket-cont">
+                <h4>You dont have any tickets.</h4>
+            </span>
+        </a>
+    <?php } ?>
+    <br>
+    <?php if (ceil($total_pages / $num_results_on_page) > 0) { ?>
+        <div class="text-center mt-70">
+            <ul class="kd-pagination mt-40">
+                <?php if ($page > 1) { ?>
+                    <li class="kd-pagination-icon"><a href="<?php echo $_CONFIG['ticketsurl']; ?>?status=<?php echo $status; ?>&page=<?php echo $page - 1; ?>"><i class="fa-solid fa-chevron-left"></i></a></li>
+                <?php }
+                if ($page > 3) { ?>
+                    <li><a href="<?php echo $_CONFIG['ticketsurl']; ?>?status=<?php echo $status; ?>&page=1">1</a></li>
+                    <li class="kd-pagination-space">...</li>
+                <?php }
+                if ($page - 2 > 0) { ?>
+                    <li><a href="<?php echo $_CONFIG['ticketsurl']; ?>?status=<?php echo $status; ?>&page=<?php echo $page - 2 ?>"><?php echo $page - 2 ?></a></li>
+                <?php }
+                if ($page - 1 > 0) { ?>
+                    <li><a href="<?php echo $_CONFIG['ticketsurl']; ?>?status=<?php echo $status; ?>&page=<?php echo $page - 1 ?>"><?php echo $page - 1 ?></a></li>
+                <?php } ?>
+                <li class="active"><a href="<?php echo $_CONFIG['ticketsurl']; ?>?status=<?php echo $status; ?>&page=<?php echo $page; ?>"><?php echo $page; ?></a></li>
+                <?php if ($page + 1 < ceil($total_pages / $num_results_on_page) + 1) { ?>
+                    <li>
+                        <a href="<?php echo $_CONFIG['ticketsurl']; ?>?status=<?php echo $status; ?>&page=<?php echo $page + 1 ?>">
+                            <?php echo $page + 1 ?>
+                        </a>
+                    </li>
+                <?php }
+                if ($page + 2 < ceil($total_pages / $num_results_on_page) + 2) { ?>
+                    <li><a href="<?php echo $_CONFIG['ticketsurl']; ?>?status=<?php echo $status; ?>&page=<?php echo $page + 2 ?>"><?php echo $page + 2 ?></a></li>
+                <?php }
+                if ($page < ceil($total_pages / $num_results_on_page) - 2) { ?>
+                    <li class="kd-pagination-space">...</li>
+                    <li>
+                        <a href="<?php echo $_CONFIG['ticketsurl']; ?>?status=<?php echo $status; ?>&page=<?php echo ceil($total_pages / $num_results_on_page) ?>"><?php echo ceil($total_pages / $num_results_on_page) ?></a>
+                    </li>
+                <?php }
+                if ($page < ceil($total_pages / $num_results_on_page)) { ?>
+                    <li class="kd-pagination-icon"><a href="<?php echo $_CONFIG['ticketsurl']; ?>?status=<?php echo $status; ?>&page=<?php echo $page + 1 ?>"><i class="fa-solid fa-chevron-right"></i></a></li>
+                <?php } ?>
+            </ul>
+        </div>
+    <?php }
+} else {
+    $accountid = $_SESSION['accountid'];
+    $total_pages = $dbcon->query("SELECT * FROM invoices WHERE accountid = '$accountid'")->num_rows;
+    $page = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1;
+    $num_results_on_page = 5;
+    if ($stmt = $dbcon->prepare("SELECT * FROM invoices WHERE accountid = ? LIMIT ?,?")) {
+        $calc_page = ($page - 1) * $num_results_on_page;
+        $stmt->bind_param('sii', $accountid, $calc_page, $num_results_on_page);
+        $stmt->execute();
+        $result = $stmt->get_result(); 
+        while ($row = $result->fetch_assoc()) { ?>
+            <a href="<?php echo $_CONFIG['ticketsurl']; ?>/ticket.php?ticketno=<?php echo $row['ticket_no']; ?>" class="kd-ticket-item kd-ticket-new kd-ticket-open kd-block-decorated">
+                <span class="kd-ticket-img">
+                    <img src="<?php echo $_CONFIG['supporturl']; ?>/assets/images/avatar-1.png" alt="">
+                </span>
+                <span class="kd-ticket-cont">
+                    <span class="kd-ticket-name"> <?php echo $row['lastreplyname']; ?> </span>
+                    <span class="kd-ticket-title h5"> <?php echo $row['lastreplymsg']; ?> </span>
+                    <ul class="kd-ticket-info">
+                        <li>Update: <?php echo $row['lastreplydate'] . ' at ' . $row['lastreplyname']; ?>; ?></li>
+                        <li>Product: <?php echo $row['ticket_product']; ?></li>
+                        <li>Comments: <?php echo $row['ticketreplys']; ?></li>
+                        <?php if ($row['lastreplyaccountid'] != $_SESSION['accountid']) { ?>
+                            <li class="kd-ticket-new">New Reply</li>
+                        <?php } ?>
+                    </ul>
+                </span>
+                <span class="kd-ticket-status">
+                    <?php
+                    if ($row['paymentstatus'] == 'PAID') {
+                        echo '<div style="color: green;">PAID</div>';
+                    } elseif ($row['paymentstatus'] == 'DUE') {
+                        echo '<div style="color: red;">DUE</div>';
+                    } elseif ($row['paymentstatus'] == 'UNPAID') {
+                        echo '<div style="color: orange;">UNPAID</div>';
+                    } else {
+                        echo '<div style="color: blue;">UNKNOWN</div>';
+                    }
+                    ?>
+                </span>
+            </a>
+        <?php } ?>
+    <?php } else { ?>
+        <a href="<?php echo $_CONFIG['ticketsurl']; ?>/new-ticket.php" class="kd-ticket-item kd-ticket-new kd-ticket-open kd-block-decorated">
+            <span class="kd-ticket-cont">
+                <h4>You dont have any tickets.</h4>
+            </span>
+        </a>
+    <?php } ?>
+    <br>
+    <?php if (ceil($total_pages / $num_results_on_page) > 0) { ?>
+        <div class="text-center mt-70">
+            <ul class="kd-pagination mt-40">
+                <?php if ($page > 1) { ?>
+                    <li class="kd-pagination-icon"><a href="<?php echo $_CONFIG['ticketsurl']; ?>?page=<?php echo $page - 1; ?>"><i class="fa-solid fa-chevron-left"></i></a></li>
+                <?php }
+                if ($page > 3) { ?>
+                    <li><a href="<?php echo $_CONFIG['ticketsurl']; ?>?page=1">1</a></li>
+                    <li class="kd-pagination-space">...</li>
+                <?php }
+                if ($page - 2 > 0) { ?>
+                    <li><a href="<?php echo $_CONFIG['ticketsurl']; ?>?page=<?php echo $page - 2 ?>"><?php echo $page - 2 ?></a></li>
+                <?php }
+                if ($page - 1 > 0) { ?>
+                    <li><a href="<?php echo $_CONFIG['ticketsurl']; ?>?page=<?php echo $page - 1 ?>"><?php echo $page - 1 ?></a></li>
+                <?php } ?>
+                <li class="active"><a href="<?php echo $_CONFIG['ticketsurl']; ?>?page=<?php echo $page; ?>"><?php echo $page; ?></a></li>
+                <?php if ($page + 1 < ceil($total_pages / $num_results_on_page) + 1) { ?>
+                    <li>
+                        <a href="<?php echo $_CONFIG['ticketsurl']; ?>?page=<?php echo $page + 1 ?>">
+                            <?php echo $page + 1 ?>
+                        </a>
+                    </li>
+                <?php }
+                if ($page + 2 < ceil($total_pages / $num_results_on_page) + 2) { ?>
+                    <li><a href="<?php echo $_CONFIG['ticketsurl']; ?>?page=<?php echo $page + 2 ?>"><?php echo $page + 2 ?></a></li>
+                <?php }
+                if ($page < ceil($total_pages / $num_results_on_page) - 2) { ?>
+                    <li class="kd-pagination-space">...</li>
+                    <li>
+                        <a href="<?php echo $_CONFIG['ticketsurl']; ?>?page=<?php echo ceil($total_pages / $num_results_on_page) ?>"><?php echo ceil($total_pages / $num_results_on_page) ?></a>
+                    </li>
+                <?php }
+                if ($page < ceil($total_pages / $num_results_on_page)) { ?>
+                    <li class="kd-pagination-icon"><a href="<?php echo $_CONFIG['ticketsurl']; ?>?page=<?php echo $page + 1 ?>"><i class="fa-solid fa-chevron-right"></i></a></li>
+                <?php } ?>
+            </ul>
+        </div>
+<?php }
+} ?>
--- support/tickets/my-tickets.php
+++ support/tickets/my-tickets.php
@@ -17,18 +17,15 @@ if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] != TRUE) { ?>
                 <li <?php if (!isset($_GET['status'])) {
                         echo 'class="active"';
                     } ?>><a href="<?php echo $_CONFIG['ticketsurl']; ?>/my-tickets.php">All Tickets</a></li>
-                <li <?php if ($_GET['status'] == 'open') {
+                <li <?php if ($_GET['status'] == 'OPEN') {
                         echo 'class="active"';
-                    } ?>><a href="<?php echo $_CONFIG['ticketsurl']; ?>/my-tickets.php?status=open">Open Tickets</a></li>
-                <li <?php if ($_GET['status'] == 'closed') {
+                    } ?>><a href="<?php echo $_CONFIG['ticketsurl']; ?>/my-tickets.php?status=open">Open</a></li>
+                <li <?php if ($_GET['status'] == 'CLOSED') {
                         echo 'class="active"';
-                    } ?>><a href="<?php echo $_CONFIG['ticketsurl']; ?>/my-tickets.php?status=closed">Closed Tickets</a></li>
-                <li <?php if ($_GET['status'] == 'onhold') {
+                    } ?>><a href="<?php echo $_CONFIG['ticketsurl']; ?>/my-tickets.php?status=closed">Closed</a></li>
+                <li <?php if ($_GET['status'] == 'ONHOLD') {
                         echo 'class="active"';
-                    } ?>><a href="<?php echo $_CONFIG['ticketsurl']; ?>/my-tickets.php?status=onhold">On Hold Tickets</a></li>
-                <li <?php if ($_GET['status'] == 'pending') {
-                        echo 'class="active"';
-                    } ?>><a href="<?php echo $_CONFIG['ticketsurl']; ?>/my-tickets.php?status=pending">Pending Tickets</a></li>
+                    } ?>><a href="<?php echo $_CONFIG['ticketsurl']; ?>/my-tickets.php?status=onhold">On Hold</a></li>
             </ul>
         </div>
     </div>
@@ -43,40 +40,12 @@ if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] != TRUE) { ?>
                     <div class="kd-separator ml-10 mr-10"></div>
                 </div>
                 <div class="col-auto">
-                    <a href="ticket-submit.php" class="kd-btn kd-btn-md">Submit a ticket</a>
+                    <a href="<?php echo $_CONFIG['ticketsurl']; ?>new-ticket.php" class="kd-btn kd-btn-md">Submit a ticket</a>
                 </div>
             </div>
-            <a href="single-ticket.php" class="kd-ticket-item kd-ticket-new kd-ticket-open kd-block-decorated">
-                <span class="kd-ticket-img">
-                    <img src="<?php echo $_CONFIG['supporturl']; ?>/assets/images/avatar-1.png" alt="">
-                </span>
-                <span class="kd-ticket-cont">
-                    <span class="kd-ticket-name"> John Leonard </span>
-                    <span class="kd-ticket-title h5"> Need help with customization. Some options are not appearing... </span>
-                    <ul class="kd-ticket-info">
-                        <li>Update: 5 Nov 2018</li>
-                        <li>Product: Quantial</li>
-                        <li>Comments: 2</li>
-                        <li class="kd-ticket-new">New</li>
-                    </ul>
-                </span>
-                <span class="kd-ticket-status"> Open </span>
-            </a>
-            <a href="single-ticket.php" class="kd-ticket-item kd-ticket-closed kd-block-decorated">
-                <span class="kd-ticket-img">
-                    <img src="<?php echo $_CONFIG['supporturl']; ?>/assets/images/avatar-default.svg" alt="">
-                </span>
-                <span class="kd-ticket-cont">
-                    <span class="kd-ticket-name"> Bruno Rice </span>
-                    <span class="kd-ticket-title h5"> Theme not updating in downloads </span>
-                    <ul class="kd-ticket-info">
-                        <li>Update: 4 Nov 2018</li>
-                        <li>Product: Sensific</li>
-                        <li>Comments: 11</li>
-                    </ul>
-                </span>
-                <span class="kd-ticket-status"> Closed </span>
-            </a>
+            <?php
+            require $_SERVER['DOCUMENT_ROOT'] . '/inc/mytickets.php';
+            ?>
         </div>
     </div>
     <div class="kd-separator"></div>
