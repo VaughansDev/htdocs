b3b6fe0a07afa5b7a374cd4d4ae374fa16e84bca
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-02-26 02:56:07 -0400

    fgn

--- account/my-invoices.php
+++ account/my-invoices.php
@@ -38,69 +38,10 @@ if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] != TRUE) { ?>
     <div class="kd-box-5 pb-100 bg-grey-6">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-xl-10">
-                    <div class="kd-box-decorated">
-                        <div class="table-responsive">
-                            <table class="kd-table kd-table-default">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Item</th>
-                                        <th scope="col">License Key</th>
-                                        <th scope="col">License</th>
-                                        <th scope="col">Website</th>
-                                        <th scope="col">Date</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row" class="kd-table-default-product">
-                                            <a href="product.php">Quantial – Moving Upon Signs Moveth Lesser</a>
-                                        </th>
-                                        <td class="kd-table-licenseKey">98vbjy-t46879ug-fd9iytvh-578g0-8ubfyj00</td>
-                                        <td>Standard</td>
-                                        <td class="kd-table-website kd-table-default-website"><a href="#">https://example.info/</a></td>
-                                        <td>21.07.2018</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row" class="kd-table-default-product">
-                                            <a href="product.php">Sensific – Saying Beast Lesser for in Fruitful</a>
-                                        </th>
-                                        <td class="kd-table-licenseKey">98vbjy-t46879ug-fd9iytvh-578g0-8ubfyj00</td>
-                                        <td>Extended</td>
-                                        <td class="kd-table-website kd-table-default-website">Not activated</td>
-                                        <td>18.07.2018</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row" class="kd-table-default-product">
-                                            <a href="product.php">Minist – Subdue Above for Signs Dry is Have Great</a>
-                                        </th>
-                                        <td class="kd-table-licenseKey">98vbjy-t46879ug-fd9iytvh-578g0-8ubfyj00</td>
-                                        <td>Standard</td>
-                                        <td class="kd-table-website kd-table-default-website"><a href="#" class="kd-btn kd-btn-md kd-btn-grey-2">Deactivate</a></td>
-                                        <td>18.07.2018</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row" class="kd-table-default-product">
-                                            <a href="product.php">Desty – She'd Under Light Over Multiply Stars Winged</a>
-                                        </th>
-                                        <td class="kd-table-licenseKey">98vbjy-t46879ug-fd9iytvh-578g0-8ubfyj00</td>
-                                        <td>Extended</td>
-                                        <td class="kd-table-website kd-table-default-website">Not activated</td>
-                                        <td>15.07.2018</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row" class="kd-table-default-product">
-                                            <a href="product.php">Silies – Grass Living Fly Subdue After Fruitful</a>
-                                        </th>
-                                        <td class="kd-table-licenseKey">98vbjy-t46879ug-fd9iytvh-578g0-8ubfyj00</td>
-                                        <td>Extended</td>
-                                        <td class="kd-table-website kd-table-default-website">Not activated</td>
-                                        <td>03.07.2018</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                <div class="col-xl-12">
+                    <?php
+                    require $_SERVER['DOCUMENT_ROOT'] . '/inc/myinvoices.php';
+                    ?>
                 </div>
             </div>
         </div>
---
+++ docs/inc/myinvoices.php
@@ -0,0 +1,206 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/inc/dbcon.php';
+if (isset($_GET['status'])) {
+    $status = $_GET['status'];
+    $accountid = $_SESSION['accountid'];
+    $total_pages = $dbcon->query("SELECT * FROM invoices WHERE accountid = '$accountid' AND payment_status = '$status'")->num_rows;
+    $page = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1;
+    $num_results_on_page = 12;
+    if ($stmt = $dbcon->prepare("SELECT * FROM licenses WHERE accountid = ? AND payment_status = ? LIMIT ?,?")) {
+        $calc_page = ($page - 1) * $num_results_on_page;
+        $stmt->bind_param('ssii', $accountid, $status, $calc_page, $num_results_on_page);
+        $stmt->execute();
+        $result = $stmt->get_result(); ?>
+        <div class="kd-box-decorated">
+            <div class="table-responsive">
+                <table class="kd-table kd-table-default">
+                    <thead>
+                        <tr>
+                            <th scope="col">Invoice #</th>
+                            <th scope="col">Invoice Date</th>
+                            <th scope="col">Invoice Total</th>
+                            <th scope="col">License Type</th>
+                            <th scope="col">Product Type</th>
+                            <th scope="col">Payment Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($row = $result->fetch_assoc()) { ?>
+                            <tr>
+                                <th scope="row" class="kd-table-default-product"><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['invoicenum']; ?></a></th>
+                                <td><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['invoicedate']; ?></a></td>
+                                <td><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['invoicetotal']; ?></a></td>
+                                <td class="kd-table-licenseKey"><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['licensetype']; ?></a></td>
+                                <td class="kd-table-licenseKey"><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['producttype']; ?></a></td>
+                                <td><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['status']; ?></a></td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    <?php } else { ?>
+        <div class="kd-box-decorated">
+            <div class="table-responsive">
+                <table class="kd-table kd-table-default">
+                    <thead>
+                        <tr>
+                            <th scope="col">Invoice #</th>
+                            <th scope="col">Invoice Date</th>
+                            <th scope="col">Invoice Totat</th>
+                            <th scope="col">License Type</th>
+                            <th scope="col">Product Type</th>
+                            <th scope="col">Payment Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <h4>No Licenses Found</h4>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    <?php } ?>
+    <br>
+    <?php if (ceil($total_pages / $num_results_on_page) > 0) { ?>
+        <div class="text-center mt-70">
+            <ul class="kd-pagination mt-40">
+                <?php if ($page > 1) { ?>
+                    <li class="kd-pagination-icon"><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page - 1; ?>"><i class="fa-solid fa-chevron-left"></i></a></li>
+                <?php }
+                if ($page > 3) { ?>
+                    <li><a href="<?php echo $_CONFIG['storeurl']; ?>?page=1">1</a></li>
+                    <li class="kd-pagination-space">...</li>
+                <?php }
+                if ($page - 2 > 0) { ?>
+                    <li><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page - 2 ?>"><?php echo $page - 2 ?></a></li>
+                <?php }
+                if ($page - 1 > 0) { ?>
+                    <li><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page - 1 ?>"><?php echo $page - 1 ?></a></li>
+                <?php } ?>
+                <li class="active"><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page; ?>"><?php echo $page; ?></a></li>
+                <?php if ($page + 1 < ceil($total_pages / $num_results_on_page) + 1) { ?>
+                    <li>
+                        <a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page + 1 ?>">
+                            <?php echo $page + 1 ?>
+                        </a>
+                    </li>
+                <?php }
+                if ($page + 2 < ceil($total_pages / $num_results_on_page) + 2) { ?>
+                    <li><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page + 2 ?>"><?php echo $page + 2 ?></a></li>
+                <?php }
+                if ($page < ceil($total_pages / $num_results_on_page) - 2) { ?>
+                    <li class="kd-pagination-space">...</li>
+                    <li>
+                        <a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo ceil($total_pages / $num_results_on_page) ?>"><?php echo ceil($total_pages / $num_results_on_page) ?></a>
+                    </li>
+                <?php }
+                if ($page < ceil($total_pages / $num_results_on_page)) { ?>
+                    <li class="kd-pagination-icon"><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page + 1 ?>"><i class="fa-solid fa-chevron-right"></i></a></li>
+                <?php } ?>
+            </ul>
+        </div>
+    <?php }
+} else {
+    $accountid = $_SESSION['accountid'];
+    $total_pages = $dbcon->query("SELECT * FROM invoices WHERE accountid = '$accountid'")->num_rows;
+    $page = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1;
+    $num_results_on_page = 5;
+    if ($stmt = $dbcon->prepare("SELECT * FROM invoices WHERE accountid = ? LIMIT ?,?")) {
+        $calc_page = ($page - 1) * $num_results_on_page;
+        $stmt->bind_param('sii', $accountid, $calc_page, $num_results_on_page);
+        $stmt->execute();
+        $result = $stmt->get_result(); ?>
+        <div class="kd-box-decorated">
+            <div class="table-responsive">
+                <table class="kd-table kd-table-default">
+                    <thead>
+                        <tr>
+                            <th scope="col">Invoice #</th>
+                            <th scope="col">Invoice Date</th>
+                            <th scope="col">Invoice Totat</th>
+                            <th scope="col">License Type</th>
+                            <th scope="col">Product Type</th>
+                            <th scope="col">Payment Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($row = $result->fetch_assoc()) { ?>
+                            <tr>
+                                <th scope="row" class="kd-table-default-product"><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['invoicenum']; ?></a></th>
+                                <td><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['invoicedate']; ?></a></td>
+                                <td><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['invoicetotal']; ?></a></td>
+                                <td class="kd-table-licenseKey"><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['licensetype']; ?></a></td>
+                                <td class="kd-table-licenseKey"><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['producttype']; ?></a></td>
+                                <td><a href="<?php echo $_CONFIG['accounturl']; ?>/invoice/view-invoice.php?invnum=<?php echo $row['invoicenum']; ?>"><?php echo $row['status']; ?></a></td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    <?php } else { ?>
+        <div class="kd-box-decorated">
+            <div class="table-responsive">
+                <table class="kd-table kd-table-default">
+                    <thead>
+                        <tr>
+                            <th scope="col">Invoice #</th>
+                            <th scope="col">Invoice Date</th>
+                            <th scope="col">Invoice Totat</th>
+                            <th scope="col">License Type</th>
+                            <th scope="col">Product Type</th>
+                            <th scope="col">Payment Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <h4>No Licenses Found</h4>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    <?php } ?>
+    <br>
+    <?php if (ceil($total_pages / $num_results_on_page) > 0) { ?>
+        <div class="text-center mt-70">
+            <ul class="kd-pagination mt-40">
+                <?php if ($page > 1) { ?>
+                    <li class="kd-pagination-icon"><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page - 1; ?>"><i class="fa-solid fa-chevron-left"></i></a></li>
+                <?php }
+                if ($page > 3) { ?>
+                    <li><a href="<?php echo $_CONFIG['storeurl']; ?>?page=1">1</a></li>
+                    <li class="kd-pagination-space">...</li>
+                <?php }
+                if ($page - 2 > 0) { ?>
+                    <li><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page - 2 ?>"><?php echo $page - 2 ?></a></li>
+                <?php }
+                if ($page - 1 > 0) { ?>
+                    <li><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page - 1 ?>"><?php echo $page - 1 ?></a></li>
+                <?php } ?>
+                <li class="active"><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page; ?>"><?php echo $page; ?></a></li>
+                <?php if ($page + 1 < ceil($total_pages / $num_results_on_page) + 1) { ?>
+                    <li>
+                        <a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page + 1 ?>">
+                            <?php echo $page + 1 ?>
+                        </a>
+                    </li>
+                <?php }
+                if ($page + 2 < ceil($total_pages / $num_results_on_page) + 2) { ?>
+                    <li><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page + 2 ?>"><?php echo $page + 2 ?></a></li>
+                <?php }
+                if ($page < ceil($total_pages / $num_results_on_page) - 2) { ?>
+                    <li class="kd-pagination-space">...</li>
+                    <li>
+                        <a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo ceil($total_pages / $num_results_on_page) ?>"><?php echo ceil($total_pages / $num_results_on_page) ?></a>
+                    </li>
+                <?php }
+                if ($page < ceil($total_pages / $num_results_on_page)) { ?>
+                    <li class="kd-pagination-icon"><a href="<?php echo $_CONFIG['storeurl']; ?>?page=<?php echo $page + 1 ?>"><i class="fa-solid fa-chevron-right"></i></a></li>
+                <?php } ?>
+            </ul>
+        </div>
+<?php }
+} ?>
