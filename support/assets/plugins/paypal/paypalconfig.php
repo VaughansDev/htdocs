cd864e26fbca6e61397b755d1d6261713416b303
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-11 00:04:45 -0300

    g

--- support/assets/plugins/paypal/paypalconfig.php
+++ support/assets/plugins/paypal/paypalconfig.php
@@ -4,17 +4,17 @@ require_once 'vendor/autoload.php';
 use Omnipay\Omnipay;
 
 $_CONFIG['dbhost'] = 'localhost';
-$_CONFIG['dbuser'] = 'root';
-$_CONFIG['dbpass'] = '55d7218283f672edb3f0';
-$_CONFIG['dbname'] = 'vaughansdev';
+$_CONFIG['dbuser'] = 'khaosdev_user';
+$_CONFIG['dbpass'] = 'fU6+IN)s~UmMhv;jNT';
+$_CONFIG['dbname'] = 'khaosdev_main';
 
 $dbcon = new mysqli($_CONFIG['dbhost'], $_CONFIG['dbuser'], $_CONFIG['dbpass'], $_CONFIG['dbname']);
 if ($dbcon->connect_errno) {
   die("Connect failed: ". $db->connect_error);
 }
 
-define('CLIENT_ID', 'AVi7SRZM8m4l7cW0qcMnmVHx5dqr5j17jg_qZA3e4GzmB8wptCcJne8tMgE1uP0678rEKKaSxkOJ75BF');
-define('CLIENT_SECRET', 'EADPa4N-LpwuuTRFIjsBldSwSZFj3UwuDExmTRWKR_SfvnoIIrKHtal2Fe51AyOC8u2gQkZWwYIiyvhR');
+define('CLIENT_ID', 'AeN0dme_TZjUilGYvXtlEpB9vNRlyJ9cjsSGRtpJR0MUs22R8YCKtYjUKgufCfaA0lkPFCVyfSfC3NK2');
+define('CLIENT_SECRET', 'EP30X3afr_2OSND2krbEL4ZfN6PaTUbe3GD5B8PQOLGIOujo-lrFwSqLWt40uZ5j4m5k7kib8ghBbMX8');
 
 define('PAYPAL_RETURN_URL', 'http://localhost/assets/plugins/paypal/paypalsuccess.php');
 define('PAYPAL_CANCEL_URL', 'http://localhost/assets/plugins/paypal/paypalcancel.php');
