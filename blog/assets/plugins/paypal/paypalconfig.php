e1aad6ec5672f4f62ddbb13657fe2bc3547936f8
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-11 00:11:33 -0300

    v

--- blog/assets/plugins/paypal/paypalconfig.php
+++ blog/assets/plugins/paypal/paypalconfig.php
@@ -16,8 +16,8 @@ if ($dbcon->connect_errno) {
 define('CLIENT_ID', 'AeN0dme_TZjUilGYvXtlEpB9vNRlyJ9cjsSGRtpJR0MUs22R8YCKtYjUKgufCfaA0lkPFCVyfSfC3NK2');
 define('CLIENT_SECRET', 'EP30X3afr_2OSND2krbEL4ZfN6PaTUbe3GD5B8PQOLGIOujo-lrFwSqLWt40uZ5j4m5k7kib8ghBbMX8');
 
-define('PAYPAL_RETURN_URL', 'http://localhost/assets/plugins/paypal/paypalsuccess.php');
-define('PAYPAL_CANCEL_URL', 'http://localhost/assets/plugins/paypal/paypalcancel.php');
+define('PAYPAL_RETURN_URL', 'http://store.khaosdevelopment.com/assets/plugins/paypal/paypalsuccess.php');
+define('PAYPAL_CANCEL_URL', 'http://store.khaosdevelopment.com/assets/plugins/paypal/paypalcancel.php');
 define('PAYPAL_CURRENCY', 'CAD');
 
 $gateway = Omnipay::create('PayPal_Rest');
