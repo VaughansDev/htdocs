e954a8662c4018715020bd72e05c3327bb59d6ff
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-14 18:44:55 -0300

    n

--- store/payment/card.php
+++ store/payment/card.php
@@ -1,160 +1,131 @@
-<link rel='stylesheet' href='../assets/css/app.css'>
-<link rel='stylesheet' href='../assets/css/dark-mode.css'>
+<link href="../assets/css/app.css" rel="stylesheet" />
 <script type="text/javascript" src="https://sandbox.web.squarecdn.com/v1/square.js"></script>
 <script>
-  const appId = 'sandbox-sq0idb-Mc0SPnKWQ3cYVJ-0EGnoiQ';
-  const locationId = 'LWGA1R7B2E4WY';
-
-  async function initializeCard(payments) {
-    const card = await payments.card();
-    await card.attach('#card-container');
-
-    return card;
-  }
-
-  <?php
-  $_SESSION['idempotencyKey'] = uniqid();
-  ?>
-  async function createPayment(token, verificationToken) {
-    const body = JSON.stringify({
-      locationId,
-      sourceId: token,
-      verificationToken,
-      idempotencyKey: '<?php echo $_SESSION['idempotencyKey']; ?>',
-    });
-    const paymentResponse = await fetch('payment/process-payment.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body,
-    });
-
-    if (paymentResponse.ok) {
-      return paymentResponse.json();
+    const appId = 'sandbox-sq0idb-Mc0SPnKWQ3cYVJ-0EGnoiQ';
+    const locationId = 'LWGA1R7B2E4WY';
+    async function initializeCard(payments) {
+        const card = await payments.card();
+        await card.attach('#card-container');
+        return card;
     }
-
-    const errorBody = await paymentResponse.text();
-    throw new Error(errorBody);
-  }
-
-  async function tokenize(paymentMethod) {
-    const tokenResult = await paymentMethod.tokenize();
-    if (tokenResult.status === 'OK') {
-      return tokenResult.token;
-    } else {
-      let errorMessage = `Tokenization failed with status: ${tokenResult.status}`;
-      if (tokenResult.errors) {
-        errorMessage += ` and errors: ${JSON.stringify(
-        tokenResult.errors
-      )}`;
-      }
-
-      throw new Error(errorMessage);
+    async function createPayment(token, verificationToken) {
+        const body = JSON.stringify({
+            locationId,
+            sourceId: token,
+            verificationToken,
+            idempotencyKey: window.crypto.randomUUID(),
+        });
+        const paymentResponse = await fetch('/payment/process-action.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body,
+        });
+        if (paymentResponse.ok) {
+            return paymentResponse.json();
+        }
+        const errorBody = await paymentResponse.text();
+        throw new Error(errorBody);
     }
-  }
-
-  //Required in SCA Mandated Regions: Learn more at https://developer.squareup.com/docs/sca-overview
-  async function verifyBuyer(payments, token) {
-    const verificationDetails = {
-      amount: '97.99',
-      billingContact: {
-        <?php if (!empty($_SESSION['address2'])) { ?>
-          addressLines: ['<?php $_SESSION['address1']; ?>', '<?php $_SESSION['address2']; ?>'],
-        <?php } else { ?>
-          addressLines: ['<?php $_SESSION['address1']; ?>'],
-        <?php } ?>
-        familyName: '<?php $_SESSION['lastname']; ?>',
-        givenName: '<?php $_SESSION['firstname']; ?>',
-        email: '<?php $_SESSION['email']; ?>',
-        country: '<?php $_SESSION['country']; ?>',
-        phone: '<?php $_SESSION['phone']; ?>',
-        region: '<?php $_SESSION['state']; ?>',
-        city: '<?php $_SESSION['city']; ?>',
-      },
-      currencyCode: 'CAD',
-      intent: 'CHARGE',
-    };
-
-    const verificationResults = await payments.verifyBuyer(
-      token,
-      verificationDetails
-    );
-    return verificationResults.token;
-  }
-
-  // status is either SUCCESS or FAILURE;
-  function displayPaymentResults(status) {
-    const statusContainer = document.getElementById(
-      'payment-status-container'
-    );
-    if (status === 'SUCCESS') {
-      statusContainer.classList.remove('is-failure');
-      statusContainer.classList.add('is-success');
-    } else {
-      statusContainer.classList.remove('is-success');
-      statusContainer.classList.add('is-failure');
+    async function tokenize(paymentMethod) {
+        const tokenResult = await paymentMethod.tokenize();
+        if (tokenResult.status === 'OK') {
+            return tokenResult.token;
+        } else {
+            let errorMessage = `Tokenization failed with status: ${tokenResult.status}`;
+            if (tokenResult.errors) {
+                errorMessage += ` and errors: ${JSON.stringify(
+          tokenResult.errors,
+        )}`;
+            }
+            throw new Error(errorMessage);
+        }
     }
-
-    statusContainer.style.visibility = 'visible';
-  }
-
-  document.addEventListener('DOMContentLoaded', async function() {
-    if (!window.Square) {
-      throw new Error('Square.js failed to load properly');
-    }
-
-    let payments;
-    try {
-      payments = window.Square.payments(appId, locationId);
-    } catch {
-      const statusContainer = document.getElementById(
-        'payment-status-container'
-      );
-      statusContainer.className = 'missing-credentials';
-      statusContainer.style.visibility = 'visible';
-      return;
-    }
-
-    let card;
-    try {
-      card = await initializeCard(payments);
-    } catch (e) {
-      console.error('Initializing Card failed', e);
-      return;
-    }
-
-    async function handlePaymentMethodSubmission(event, card) {
-      event.preventDefault();
-
-      try {
-        // disable the submit button as we await tokenization and make a payment request.
-        cardButton.disabled = true;
-        const token = await tokenize(card);
-        const verificationToken = await verifyBuyer(payments, token);
-        const paymentResults = await createPayment(
-          token,
-          verificationToken
+    // Required in SCA Mandated Regions: Learn more at https://developer.squareup.com/docs/sca-overview
+    async function verifyBuyer(payments, token) {
+        const verificationDetails = {
+            amount: '1.00',
+            billingContact: {
+                givenName: 'John',
+                familyName: 'Doe',
+                email: 'john.doe@square.example',
+                phone: '3214563987',
+                addressLines: ['123 Main Street', 'Apartment 1'],
+                city: 'London',
+                state: 'LND',
+                countryCode: 'GB',
+            },
+            currencyCode: 'GBP',
+            intent: 'CHARGE',
+        };
+        const verificationResults = await payments.verifyBuyer(
+            token,
+            verificationDetails,
         );
-        displayPaymentResults('SUCCESS');
-
-        console.debug('Payment Success', paymentResults);
-      } catch (e) {
-        cardButton.disabled = false;
-        displayPaymentResults('FAILURE');
-        console.error(e.message);
-      }
+        return verificationResults.token;
     }
-
-    const cardButton = document.getElementById('card-button');
-    cardButton.addEventListener('click', async function(event) {
-      await handlePaymentMethodSubmission(event, card);
+    // status is either SUCCESS or FAILURE;
+    function displayPaymentResults(status) {
+        const statusContainer = document.getElementById(
+            'payment-status-container',
+        );
+        if (status === 'SUCCESS') {
+            statusContainer.classList.remove('is-failure');
+            statusContainer.classList.add('is-success');
+        } else {
+            statusContainer.classList.remove('is-success');
+            statusContainer.classList.add('is-failure');
+        }
+        statusContainer.style.visibility = 'visible';
+    }
+    document.addEventListener('DOMContentLoaded', async function() {
+        if (!window.Square) {
+            throw new Error('Square.js failed to load properly');
+        }
+        let payments;
+        try {
+            payments = window.Square.payments(appId, locationId);
+        } catch {
+            const statusContainer = document.getElementById(
+                'payment-status-container',
+            );
+            statusContainer.className = 'missing-credentials';
+            statusContainer.style.visibility = 'visible';
+            return;
+        }
+        let card;
+        try {
+            card = await initializeCard(payments);
+        } catch (e) {
+            console.error('Initializing Card failed', e);
+            return;
+        }
+        async function handlePaymentMethodSubmission(event, card) {
+            event.preventDefault();
+            try {
+                // disable the submit button as we await tokenization and make a payment request.
+                cardButton.disabled = true;
+                const token = await tokenize(card);
+                const verificationToken = await verifyBuyer(payments, token);
+                const paymentResults = await createPayment(
+                    token,
+                    verificationToken,
+                );
+                displayPaymentResults('SUCCESS');
+                console.debug('Payment Success', paymentResults);
+            } catch (e) {
+                cardButton.disabled = false;
+                displayPaymentResults('FAILURE');
+                console.error(e.message);
+            }
+        }
+        const cardButton = document.getElementById('card-button');
+        cardButton.addEventListener('click', async function(event) {
+            await handlePaymentMethodSubmission(event, card);
+        });
     });
-  });
 </script>
-<?php echo $_SESSION['idempotencyKey']; ?>
-<form action="payment/process-payment.php" id="payment-form" style="height: 100px;">
-  <div id="card-container"></div>
-  <button id="card-button" class="kd-btn kd-btn-xl kd-btn-block payment" type="button">Purchase $98</button>
-</form>
+<div id="card-container"></div>
+<button id="card-button" type="button">Pay $1.00</button>
 <div id="payment-status-container"></div>
---
+++ store/payment/process-action.php
@@ -0,0 +1,40 @@
+<?php
+require '../assets/plugins/square/vendor/autoload.php';
+
+use Square\SquareClient;
+use Square\LocationsApi;
+use Square\Exceptions\ApiException;
+use Square\Http\ApiResponse;
+use Square\Models\ListLocationsResponse;
+use Square\Environment;
+use Square\Models\Money;
+use Square\Models\CreatePaymentRequest;
+use Square\Models\CreatePayment;
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$square_client = new SquareClient ([
+    'accessToken' => 'EAAAlzQbywdmmB1zTn_ObsKPZoSdOzw8W1Gnh6H-_-oUbrae08CQ-TUl4DGrUOCD',
+    'environment' => Environment::SANDBOX,
+]);
+
+$payments_api = $square_client->getPaymentsApi();
+
+$money = new Money();
+$money->setAmount(100);
+$money->setCurrency("CAD");
+
+// Every payment you process with the SDK must have a unique idempotency key.
+// If you're unsure whether a particular payment succeeded, you can reattempt
+// it with the same idempotency key without worrying about double charging
+// the buyer.
+$orderId = uniqid();
+$create_payment_request = new CreatePaymentRequest($data['sourceId'], $orderId, $money);
+
+$response = $payments_api->createPayment($create_payment_request);
+
+if ($response->isSuccess()) {
+    echo json_encode($response->getResult());
+} else {
+    echo json_encode($response->getErrors());
+}
