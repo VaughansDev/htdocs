f2da5d435b99285a7cf697f3ae80fef9e8b879c1
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-14 09:37:20 -0300

    o

---
+++ account/widgets/newsletter.php
@@ -0,0 +1,12 @@
+<div class="kd-widget kd-box kd-box-decorated">
+    <div class="kd-widget-title"> Subscribe to Newsletter </div>
+    <div class="kd-widget-subscribe">
+        <div class="kd-widget-text">
+            <p>Join our newsletter to receive news, updates, new products and freebies in your inbox.</p>
+        </div>
+        <form action="<?php echo $_CONFIG['domain']; ?>/inc/mailinglist.php" method="post" class="kd-form kd-form-group-inputs">
+            <input type="email" name="email" aria-describedby="emailHelp" class="form-control form-control-style-2" placeholder="Your Email Address">
+            <button name="submit" type="submit" value="submit" class="kd-btn kd-btn-lg kd-btn-icon"><span class="icon fas fa-paper-plane"></span></button>
+        </form>
+    </div>
+</div>
