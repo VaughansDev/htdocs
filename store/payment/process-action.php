11d39ac6f1f89f5f9d70705f1dfce624ed1ad43b
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-14 19:09:09 -0300

    Update process-action.php

--- store/payment/process-action.php
+++ store/payment/process-action.php
@@ -35,8 +35,8 @@ $response = $payments_api->createPayment($create_payment_request);
 
 if ($response->isSuccess()) {
     echo json_encode($response->getResult());
-    header("location: ../success.php");
+    header("location: ../index.php");
 } else {
     echo json_encode($response->getErrors());
-    header("location: ../success.php");
+    header("location: ../item.php");
 }
