4863ab688c7e8ad86cb8c8834450ac03a53f5f5f
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-20 15:52:39 -0300

    s

--- forums/inc/mailinglist.php
+++ forums/inc/mailinglist.php
@@ -3,8 +3,13 @@ if (isset($_POST['submit']) && $_POST['submit'] == 'submit') {
     if (isset($_POST['email'])) {
         require 'dbcon.php';
         $email = $_POST['email'];
-        $sql = "INSERT INTO mailing_list (email) VALUES ('$email')";
-        if ($result = $dbcon->query($sql)) {
+        $sql = "SELECT * FROM mailing_list WHERE email = '$email'";
+        $result = $dbcon->query($sql);
+        if ($result->num_rows > 0) {
+            header("location: " . $_CONFIG['domain'] . "/home.php?mailist=emailAlreadyRegistered");
+        }
+        $sql2 = "INSERT INTO mailing_list (email) VALUES ('$email')";
+        if ($result2 = $dbcon->query($sql2)) {
             header("location: " . $_CONFIG['domain'] . "/home.php?mailist=success");
         } else {
             header("location: " . $_CONFIG['domain'] . "/home.php?mailist=error");
