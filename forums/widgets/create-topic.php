9838eb38a738ff6137466be37f63c0ea9c79e279
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-20 15:33:35 -0300

    Update create-topic.php

--- forums/widgets/create-topic.php
+++ forums/widgets/create-topic.php
@@ -1,5 +1,5 @@
 <div class="kd-widget kd-box kd-box-decorated">
     <div class="col-auto">
-        <a href="<?php echo $_CONFIG['forumsurl']; ?>/create-topic.php?threadId=<?php echo $_GET['threadId']; ?>" class="kd-btn kd-btn-md">Create New Topic</a>
+        <a href="<?php echo $_CONFIG['forumsurl']; ?>/create-topic.php?threadId=<?php echo $_GET['threadId']; ?>" class="kd-btn kd-btn-md" style="color: white; width: auto;">Create New Topic</a>
     </div>
 </div>
