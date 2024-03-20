618cf4200f960511b4d9c4fa0a3480530c0c1138
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-20 03:31:13 -0300

    Update latest-topics.php

--- forums/widgets/latest-topics.php
+++ forums/widgets/latest-topics.php
@@ -1,15 +1,21 @@
 <div class="kd-widget kd-box kd-box-decorated">
     <div class="kd-widget-title"> Latest Topics </div>
-    <a href="single-article.php" class="kd-widget-link">
-        <span class="kd-widget-link-text">Need help with customization. Some options are not appearing...</span>
-        <span class="kd-widget-link-date">6 Sep 2018</span>
-    </a>
-    <a href="single-article.php" class="kd-widget-link">
-        <span class="kd-widget-link-text">My images on profile and item pages doesnt show up?! Whats the matter?</span>
-        <span class="kd-widget-link-date">2 Sep 2018</span>
-    </a>
-    <a href="single-article.php" class="kd-widget-link">
-        <span class="kd-widget-link-text">Theme not updating in downloads</span>
-        <span class="kd-widget-link-date">27 Aug 2018</span>
-    </a>
+    <?php
+    require $_SERVER['DOCUMENT_ROOT'] . '/inc/dbcon.php';
+
+    $sql = "SELECT * FROM forum_topics DESC";
+    $result = $dbcon->query($sql);
+    if ($result->num_rows > 0) { 
+        while ($row = $result->fetch_assoc()) { ?>
+        <a href="single-article.php" class="kd-widget-link">
+            <span class="kd-widget-link-text"><?php echo $row['topic_title'];?></span>
+            <span class="kd-widget-link-date"><?php echo $row['topic_post_date'];?></span>
+        </a>
+    <?php } 
+    } else { ?>
+        <a href="single-article.php" class="kd-widget-link">
+            <span class="kd-widget-link-text">No new topics found</span>
+        </a>
+    <?php } 
+    $dbcon->close(); ?>
 </div>
