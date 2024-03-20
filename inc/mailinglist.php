63d52b4d6e6de50e17d6b3bf746cba9be6d2e03f
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-20 15:48:28 -0300

    a

--- inc/mailinglist.php
+++ inc/mailinglist.php
@@ -4,15 +4,14 @@ if (isset($_POST['submit']) && $_POST['submit'] == 'submit') {
         require 'dbcon.php';
         $email = $_POST['email'];
         $sql = "INSERT INTO mailing_list (email) VALUES ('$email')";
-        $result = $dbcon->query($sql);
-        if ($result) {
+        if ($result = $dbcon->query($sql)) {
             header("location: " . $_CONFIG['domain'] . "/home.php?mailist=success");
         } else {
-            header("location: ".$_CONFIG['domain']."/home.php?mailist=error");
+            header("location: " . $_CONFIG['domain'] . "/home.php?mailist=error");
         }
     } else {
         header("location: " . $_CONFIG['domain'] . "/home.php?mailist=error");
     }
 } else {
-    header("location: ".$_CONFIG['domain']."/home.php?mailist=error");
+    header("location: " . $_CONFIG['domain'] . "/home.php?mailist=error");
 }
