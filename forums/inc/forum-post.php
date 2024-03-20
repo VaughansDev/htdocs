37d9bc00ea460fd89a43eb062147f1c74154a6f5
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-20 17:16:00 -0300

    Update forum-post.php

--- forums/inc/forum-post.php
+++ forums/inc/forum-post.php
@@ -44,7 +44,11 @@ if ($result->num_rows > 0) {
                 </div>
                 <div class="kd-comment-cont">
                     <div class="kd-comment-head">
-                        <a href="javascript:;" class="kd-comment-name"><?php if ($row['post_username'] == 'VaughanHD') { echo '<span style="color: #FF0000;"><strong>[ADMIN]</strong> </span>'.$row['post_username']; } else { echo $row['post_username'];} ?></a>
+                        <a href="javascript:;" class="kd-comment-name"><?php if ($row['post_username'] == 'VaughanHD') {
+                                                                            echo '<span style="color: #FF0000;"><strong>[ADMIN]</strong> </span>' . $row['post_username'];
+                                                                        } else {
+                                                                            echo $row['post_username'];
+                                                                        } ?></a>
                     </div>
                     <div class="kd-comment-date"><?php echo $row['post_create_date'] . ', ' . $row['post_create_time']; ?></div>
                     <div class="kd-comment-text">
@@ -79,7 +83,53 @@ if ($result->num_rows > 0) {
     <form class="kd-form" action="#">
         <div class="kd-form-group">
             <div class="kd-editor-quill">
-                <div class="kd-editor" data-editor-height="150" data-editor-maxHeight="250"></div>
+                <div class="ql-toolbar ql-snow"><span class="ql-formats"><button type="button" class="ql-bold"><svg viewBox="0 0 18 18">
+                                <path class="ql-stroke" d="M5,4H9.5A2.5,2.5,0,0,1,12,6.5v0A2.5,2.5,0,0,1,9.5,9H5A0,0,0,0,1,5,9V4A0,0,0,0,1,5,4Z"></path>
+                                <path class="ql-stroke" d="M5,9h5.5A2.5,2.5,0,0,1,13,11.5v0A2.5,2.5,0,0,1,10.5,14H5a0,0,0,0,1,0,0V9A0,0,0,0,1,5,9Z"></path>
+                            </svg></button><button type="button" class="ql-italic"><svg viewBox="0 0 18 18">
+                                <line class="ql-stroke" x1="7" x2="13" y1="4" y2="4"></line>
+                                <line class="ql-stroke" x1="5" x2="11" y1="14" y2="14"></line>
+                                <line class="ql-stroke" x1="8" x2="10" y1="14" y2="4"></line>
+                            </svg></button><button type="button" class="ql-underline"><svg viewBox="0 0 18 18">
+                                <path class="ql-stroke" d="M5,3V9a4.012,4.012,0,0,0,4,4H9a4.012,4.012,0,0,0,4-4V3"></path>
+                                <rect class="ql-fill" height="1" rx="0.5" ry="0.5" width="12" x="3" y="15"></rect>
+                            </svg></button><button type="button" class="ql-strike"><svg viewBox="0 0 18 18">
+                                <line class="ql-stroke ql-thin" x1="15.5" x2="2.5" y1="8.5" y2="9.5"></line>
+                                <path class="ql-fill" d="M9.007,8C6.542,7.791,6,7.519,6,6.5,6,5.792,7.283,5,9,5c1.571,0,2.765.679,2.969,1.309a1,1,0,0,0,1.9-.617C13.356,4.106,11.354,3,9,3,6.2,3,4,4.538,4,6.5a3.2,3.2,0,0,0,.5,1.843Z"></path>
+                                <path class="ql-fill" d="M8.984,10C11.457,10.208,12,10.479,12,11.5c0,0.708-1.283,1.5-3,1.5-1.571,0-2.765-.679-2.969-1.309a1,1,0,1,0-1.9.617C4.644,13.894,6.646,15,9,15c2.8,0,5-1.538,5-3.5a3.2,3.2,0,0,0-.5-1.843Z"></path>
+                            </svg></button></span><span class="ql-formats"><button type="button" class="ql-clean"><svg class="" viewBox="0 0 18 18">
+                                <line class="ql-stroke" x1="5" x2="13" y1="3" y2="3"></line>
+                                <line class="ql-stroke" x1="6" x2="9.35" y1="12" y2="3"></line>
+                                <line class="ql-stroke" x1="11" x2="15" y1="11" y2="15"></line>
+                                <line class="ql-stroke" x1="15" x2="11" y1="11" y2="15"></line>
+                                <rect class="ql-fill" height="1" rx="0.5" ry="0.5" width="7" x="2" y="14"></rect>
+                            </svg></button></span><span class="ql-formats"><button type="button" class="ql-list" value="ordered"><svg viewBox="0 0 18 18">
+                                <line class="ql-stroke" x1="7" x2="15" y1="4" y2="4"></line>
+                                <line class="ql-stroke" x1="7" x2="15" y1="9" y2="9"></line>
+                                <line class="ql-stroke" x1="7" x2="15" y1="14" y2="14"></line>
+                                <line class="ql-stroke ql-thin" x1="2.5" x2="4.5" y1="5.5" y2="5.5"></line>
+                                <path class="ql-fill" d="M3.5,6A0.5,0.5,0,0,1,3,5.5V3.085l-0.276.138A0.5,0.5,0,0,1,2.053,3c-0.124-.247-0.023-0.324.224-0.447l1-.5A0.5,0.5,0,0,1,4,2.5v3A0.5,0.5,0,0,1,3.5,6Z"></path>
+                                <path class="ql-stroke ql-thin" d="M4.5,10.5h-2c0-.234,1.85-1.076,1.85-2.234A0.959,0.959,0,0,0,2.5,8.156"></path>
+                                <path class="ql-stroke ql-thin" d="M2.5,14.846a0.959,0.959,0,0,0,1.85-.109A0.7,0.7,0,0,0,3.75,14a0.688,0.688,0,0,0,.6-0.736,0.959,0.959,0,0,0-1.85-.109"></path>
+                            </svg></button><button type="button" class="ql-list" value="bullet"><svg viewBox="0 0 18 18">
+                                <line class="ql-stroke" x1="6" x2="15" y1="4" y2="4"></line>
+                                <line class="ql-stroke" x1="6" x2="15" y1="9" y2="9"></line>
+                                <line class="ql-stroke" x1="6" x2="15" y1="14" y2="14"></line>
+                                <line class="ql-stroke" x1="3" x2="3" y1="4" y2="4"></line>
+                                <line class="ql-stroke" x1="3" x2="3" y1="9" y2="9"></line>
+                                <line class="ql-stroke" x1="3" x2="3" y1="14" y2="14"></line>
+                            </svg></button></span><span class="ql-formats"><button type="button" class="ql-link"><svg viewBox="0 0 18 18">
+                                <line class="ql-stroke" x1="7" x2="11" y1="7" y2="11"></line>
+                                <path class="ql-even ql-stroke" d="M8.9,4.577a3.476,3.476,0,0,1,.36,4.679A3.476,3.476,0,0,1,4.577,8.9C3.185,7.5,2.035,6.4,4.217,4.217S7.5,3.185,8.9,4.577Z"></path>
+                                <path class="ql-even ql-stroke" d="M13.423,9.1a3.476,3.476,0,0,0-4.679-.36,3.476,3.476,0,0,0,.36,4.679c1.392,1.392,2.5,2.542,4.679.36S14.815,10.5,13.423,9.1Z"></path>
+                            </svg></button></span></div>
+                <div class="kd-editor ql-container ql-snow" data-editor-height="150" data-editor-maxheight="250" style="min-height: 150px; max-height: 250px;">
+                    <div class="ql-editor ql-blank" data-gramm="false" contenteditable="true" spellcheck="false">
+                        <p><br></p>
+                    </div><grammarly-extension data-grammarly-shadow-root="true" style="position: absolute; top: 0px; left: -1px; pointer-events: none; z-index: auto;" class="dnXmp"></grammarly-extension><grammarly-extension data-grammarly-shadow-root="true" style="position: absolute; top: 0px; left: -1px; pointer-events: none; z-index: auto;" class="dnXmp"></grammarly-extension>
+                    <div class="ql-clipboard" contenteditable="true" tabindex="-1"></div>
+                    <div class="ql-tooltip ql-hidden"><a class="ql-preview" rel="noopener noreferrer" target="_blank" href="about:blank"></a><input type="text" data-formula="e=mc^2" data-link="https://quilljs.com" data-video="Embed URL"><a class="ql-action"></a><a class="ql-remove"></a></div>
+                </div>
             </div>
         </div>
         <div class="kd-form-group">
@@ -88,7 +138,6 @@ if ($result->num_rows > 0) {
                     <button class="kd-btn kd-btn-lg" type="button" name="button">Post Reply</button>
                 </div>
             </div>
-            <!-- END: Dropzone -->
         </div>
     </form>
 </div>
