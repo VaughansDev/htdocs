4241e97ad42a78ad07e080f4297859ade15c2cbe
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-20 06:54:13 -0300

    Update latest-topics.php

--- forums/widgets/latest-topics.php
+++ forums/widgets/latest-topics.php
@@ -3,7 +3,7 @@
     <?php
     require $_SERVER['DOCUMENT_ROOT'] . '/inc/dbcon.php';
 
-    $sql = "SELECT * FROM forum_topics DESC";
+    $sql = "SELECT * FROM forum_topics ORDER BY id DESC";
     $result = $dbcon->query($sql);
     if ($result->num_rows > 0) { 
         while ($row = $result->fetch_assoc()) { ?>
