b94fc7e9489fb59060ebbe03c50375d4b2398fb8
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-20 15:32:12 -0300

    Update topics.php

--- forums/topics.php
+++ forums/topics.php
@@ -33,6 +33,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/inc/header.php';
                 <div class="col-lg-4">
                     <div class="kd-sticky kd-sidebar" data-sticky-offsetTop="120" data-sticky-offsetBot="40">
                         <?php
+                        require $_SERVER['DOCUMENT_ROOT'] . '/widgets/create-topic.php';
                         require $_SERVER['DOCUMENT_ROOT'] . '/widgets/search.php';
                         require $_SERVER['DOCUMENT_ROOT'] . '/widgets/latest-topics.php';
                         require $_SERVER['DOCUMENT_ROOT'] . '/widgets/newsletter.php';
