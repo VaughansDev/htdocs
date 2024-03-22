6c55cc86c87d8e4cce85a188cea0818b95ad0e74
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-22 11:53:08 -0300

    Update forum-post.php

--- forums/inc/forum-post.php
+++ forums/inc/forum-post.php
@@ -84,7 +84,49 @@ if ($result->num_rows > 0) {
     <h3 class="h6 mb-25">Write a Reply</h3>
     <form class="kd-form" method="post" action="<?php echo $_CONFIG['forumsurl']; ?>/inc/post-reply.php">
         <div class="kd-form-group">
-            <div id="toolbar"></div>
+            <div id="toolbar">
+                <span class="ql-formats">
+                    <select class="ql-size"></select>
+                </span>
+                <span class="ql-formats">
+                    <button class="ql-bold"></button>
+                    <button class="ql-italic"></button>
+                    <button class="ql-underline"></button>
+                    <button class="ql-strike"></button>
+                </span>
+                <span class="ql-formats">
+                    <select class="ql-color"></select>
+                    <select class="ql-background"></select>
+                </span>
+                <span class="ql-formats">
+                    <button class="ql-script" value="sub"></button>
+                    <button class="ql-script" value="super"></button>
+                </span>
+                <span class="ql-formats">
+                    <button class="ql-header" value="1"></button>
+                    <button class="ql-header" value="2"></button>
+                    <button class="ql-blockquote"></button>
+                    <button class="ql-code-block"></button>
+                </span>
+                <span class="ql-formats">
+                    <button class="ql-list" value="ordered"></button>
+                    <button class="ql-list" value="bullet"></button>
+                    <button class="ql-indent" value="-1"></button>
+                    <button class="ql-indent" value="+1"></button>
+                </span>
+                <span class="ql-formats">
+                    <button class="ql-direction" value="rtl"></button>
+                    <select class="ql-align"></select>
+                </span>
+                <span class="ql-formats">
+                    <button class="ql-link"></button>
+                    <button class="ql-image"></button>
+                    <button class="ql-video"></button>
+                </span>
+                <span class="ql-formats">
+                    <button class="ql-clean"></button>
+                </span>
+            </div>
             <div id="editor"></div>
         </div>
         <div class="kd-form-group">
@@ -98,11 +140,12 @@ if ($result->num_rows > 0) {
     </form>
 </div>
 <script>
-    const quill = new Quill('#editor', {
+    const options = {
         theme: 'snow',
         modules: {
             toolbar: '#toolbar'
         }
-    });
+    };
+    const quill = new Quill('#editor', options);
 </script>
 <?php $dbcon->close(); ?>
