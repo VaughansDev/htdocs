221561d43acc906b58a3d73dad9810567671c414
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-22 13:08:58 -0300

    Update post-reply.php

--- forums/inc/post-reply.php
+++ forums/inc/post-reply.php
@@ -1,4 +1,6 @@
 <?php
+require 'dbcon.php';
+
 $threadId = $_POST['threadId'];
 $topicId = $_POST['topicId'];
 $username = $_POST['username'];
@@ -8,9 +10,7 @@ $replyContent = $_POST['replyContent'];
 $replyDate = date('j M Y');
 $replyTime = date('g:i a');
 
-if (isset($_POST['submit']) && $_POST['submit'] == "submit-reply") {
-    require 'dbcon.php';
-    
+if (isset($_POST['submit']) && $_POST['submit'] == "submit-reply") {    
     $sql = "INSERT INTO forum_posts SET (post_topic_id, post_thread_id, post_profile_pic, post_create_date, post_create_time, post_username, post_account_id, post_content) VALUES ('$topicId', '$threadId', '$profilePic', '$replyDate', '$replyTime', '$username', '$accountId', '$replyContent')";
     $result = $dbcon->query($sql);
     if ($result) {
