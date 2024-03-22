a64434d10b2381f94a4adf6198a153b11cd45272
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-22 15:54:59 -0300

    gftd

--- support/tickets/new-ticket.php
+++ support/tickets/new-ticket.php
@@ -1,8 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/inc/header.php';
 if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] != TRUE) { ?>
@@ -63,7 +58,9 @@ if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] != TRUE) { ?>
                                 </div>
                                 <div class="kd-form-group">
                                     <label class="mnt-7">Ticket Message</label>
-                                    <textarea style="padding-right:20px;" name="ticketMessage" id="editor"></textarea>
+                                    <?php
+                                        require $_SERVER['DOCUMENT_ROOT'].'/inc/ticket-msg-box.php';
+                                    ?>
                                 </div>
                             </div>
                         </form>
@@ -104,21 +101,3 @@ if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] != TRUE) { ?>
     <?php
     require $_SERVER['DOCUMENT_ROOT'] . '/inc/footer.php';
     ?>
-
-    <script>
-        $(document).ready(function() {
-            $('#editor').summernote({
-                toolbar: [
-                    ['style', ['bold', 'italic', 'underline', 'clear']],
-                    ['font', ['strikethrough', 'superscript', 'subscript']],
-                    ['fontsize', ['fontsize']],
-                    ['color', ['color']],
-                    ['para', ['ul', 'ol', 'paragraph']],
-                    ['height', ['height']]
-                ],
-                placeholder: 'Type your reply here...',
-                tabsize: 2,
-                height: 200
-            });
-        });
-    </script>
