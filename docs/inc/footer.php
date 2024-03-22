845f45014aa9ba1c4a980255fdf65c8c35257ed2
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-22 14:59:30 -0300

    x

--- docs/inc/footer.php
+++ docs/inc/footer.php
@@ -92,39 +92,39 @@
 </div>
 <!-- START: Scripts -->
 <!-- Object Fit Images -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/object-fit-images/dist/ofi.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/object-fit-images/dist/ofi.min.js"></script>
 <!-- Popper -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/popper.js/dist/umd/popper.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/popper.js/dist/umd/popper.min.js"></script>
 <!-- Bootstrap -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- Fancybox -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/fancybox/dist/jquery.fancybox.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/fancybox/dist/jquery.fancybox.min.js"></script>
 <!-- Cleave -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/cleave.js/dist/cleave.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/cleave.js/dist/cleave.min.js"></script>
 <!-- Validator -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/validator/validator.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/validator/validator.min.js"></script>
 <!-- Sticky Kit -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/sticky-kit/dist/sticky-kit.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/sticky-kit/dist/sticky-kit.min.js"></script>
 <!-- Jarallax -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/jarallax/dist/jarallax.min.js"></script>
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/jarallax/dist/jarallax-video.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/jarallax/dist/jarallax.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/jarallax/dist/jarallax-video.min.js"></script>
 <!-- Isotope -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/isotope-layout/dist/isotope.pkgd.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/isotope-layout/dist/isotope.pkgd.min.js"></script>
 <!-- ImagesLoaded -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
 <!-- Swiper -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/swiper/swiper-bundle.min.js"></script>
 <!-- Gist Embed -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/gist-embed/dist/gist-embed.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/gist-embed/dist/gist-embed.min.js"></script>
 <!-- Bootstrap Select -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
 <!-- Dropzone -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/dropzone/dist/min/dropzone.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/dropzone/dist/min/dropzone.min.js"></script>
 <!-- Quill -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/vendor/quill/dist/quill.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/vendor/quill/dist/quill.min.js"></script>
 <!-- The Amdesk -->
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/js/khaos.min.js"></script>
-<script src="<?php echo $_CONFIG['domain']; ?>/assets/js/khaos-init.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/js/khaos.min.js"></script>
+<script src="<?php echo $_CONFIG['docsurl']; ?>/assets/js/khaos-init.js"></script>
 <!-- END: Scripts -->
 </body>
 
