efd9b56ecfc7a8f11e4887830bb047f84c574eea
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-22 11:57:34 -0300

    Update forum-post.php

--- forums/inc/forum-post.php
+++ forums/inc/forum-post.php
@@ -127,7 +127,7 @@ if ($result->num_rows > 0) {
                     <button class="ql-clean"></button>
                 </span>
             </div>
-            <div id="editor" style="height: 100px;"></div>
+            <div id="editor" style="height: 250px;"></div>
         </div>
         <div class="kd-form-group">
             <div class="row justify-content-between vertical-gap kd-dropzone-attachment">
