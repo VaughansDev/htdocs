345bd4fbf755d7152d4f9e37a6b9cbc214fbc2ec
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-22 13:08:05 -0300

    Update post-reply.php

--- forums/inc/post-reply.php
+++ forums/inc/post-reply.php
@@ -1,14 +1,15 @@
 <?php
+$threadId = $_POST['threadId'];
+$topicId = $_POST['topicId'];
+$username = $_POST['username'];
+$accountId = $_POST['AccountId'];
+$profilePic = $_POST['profilePic'];
+$replyContent = $_POST['replyContent'];
+$replyDate = date('j M Y');
+$replyTime = date('g:i a');
+
 if (isset($_POST['submit']) && $_POST['submit'] == "submit-reply") {
     require 'dbcon.php';
-    $threadId = $_POST['threadId'];
-    $topicId = $_POST['topicId'];
-    $username = $_POST['username'];
-    $accountId = $_POST['AccountId'];
-    $profilePic = $_POST['profilePic'];
-    $replyContent = $_POST['replyContent'];
-    $replyDate = date('j M Y');
-    $replyTime = date('g:i a');
     
     $sql = "INSERT INTO forum_posts SET (post_topic_id, post_thread_id, post_profile_pic, post_create_date, post_create_time, post_username, post_account_id, post_content) VALUES ('$topicId', '$threadId', '$profilePic', '$replyDate', '$replyTime', '$username', '$accountId', '$replyContent')";
     $result = $dbcon->query($sql);
