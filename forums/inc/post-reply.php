96ae45c93534cf4ca1d01373dce51371f7f71aa3
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-22 13:31:45 -0300

    Update post-reply.php

--- forums/inc/post-reply.php
+++ forums/inc/post-reply.php
@@ -1,6 +1,7 @@
 <?php
 require 'dbcon.php';
 
+$postId = date('dmyhis');
 $threadId = $_POST['threadId'];
 $topicId = $_POST['topicId'];
 $username = $_POST['username'];
@@ -11,7 +12,7 @@ $replyDate = date('j M Y');
 $replyTime = date('g:i a');
 
 if (isset($_POST['submit']) && $_POST['submit'] == "submit-reply") {    
-    $sql = "INSERT INTO forum_posts (post_topic_id, post_thread_id, post_profile_pic, post_create_date, post_create_time, post_username, post_account_id, post_content) VALUES ('$topicId', '$threadId', '$profilePic', '$replyDate', '$replyTime', '$username', '$accountId', '$replyContent')";
+    $sql = "INSERT INTO forum_posts (post_id, post_topic_id, post_thread_id, post_profile_pic, post_create_date, post_create_time, post_username, post_account_id, post_content) VALUES ('$postId', '$topicId', '$threadId', '$profilePic', '$replyDate', '$replyTime', '$username', '$accountId', '$replyContent')";
     $result = $dbcon->query($sql);
     if ($result) {
         header("location: " . $_CONFIG['forumsurl'] . "/topic.php?threadId=" . $threadId . "&topicId=" . $topicId."&postStatus=success");
