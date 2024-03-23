07382fade569d78f60e1b24179d709b7db0c60e8
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-23 16:23:04 -0300

    Update login.php

--- account/inc/login.php
+++ account/inc/login.php
@@ -47,7 +47,7 @@ if (isset($_GET['method']) && $_GET['method'] == 'discord') {
                     $_SESSION['auth'] = $users_auth;
                     $_SESSION['loggedin'] = TRUE;
 
-                    echo 'test';
+                    echo 'debug';
                     header("location: " . $_CONFIG['accounturl']);
                 } else {
                     $msg = urlencode("No account was found with that username.");
@@ -66,20 +66,20 @@ if (isset($_GET['method']) && $_GET['method'] == 'discord') {
     $stmt->close();
     mysqli_close($dbcon);
 } elseif (isset($_POST['login']) && $_POST['login'] == 'login') {
-    echo 'test1';
+    echo 'debug1';
     $username = $_POST['username'];
     $password = $_POST['password'];
     if ($stmt = $dbcon->prepare('SELECT * FROM users WHERE users_username = ?')) {
-        echo 'test2';
+        echo 'debug2';
         $stmt->bind_param('s', $username);
         $stmt->execute();
         $stmt->store_result();
         if ($stmt->num_rows > 0) {
-            echo 'test3';
+            echo 'debug3';
             $stmt->bind_result($users_id, $users_username, $users_firstname, $users_lastname, $users_email, $users_emailverified, $users_accountid, $users_regdate, $users_profilepic, $users_password, $users_lastlogin, $users_dicordlinked, $users_address1, $users_address2, $users_country, $users_city, $users_state, $users_postal, $users_phone, $users_auth);
             $stmt->fetch();
             if (password_verify($_POST['password'], $users_password)) {
-                echo 'test4';
+                echo 'debug4';
                 $thislogin = date("Y/m/d - h:i:sa");
                 $stmt2 = $dbcon->prepare("UPDATE users SET users_lastlogin = '$thislogin' WHERE users_username = '$username'");
                 $stmt2->execute();
@@ -110,7 +110,7 @@ if (isset($_GET['method']) && $_GET['method'] == 'discord') {
                 $_SESSION['phone'] = $users_phone;
                 $_SESSION['auth'] = $users_auth;
                 $_SESSION['loggedin'] = TRUE;
-                echo 'test5';
+                echo 'debug5';
                 header("location: " . $_CONFIG['accounturl']);
             }
         } else {
