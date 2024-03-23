afb3bc6741b98cade2143c44ad09c7c16d4318e0
VaughansDev <90429258+VaughansDev@users.noreply.github.com>
2024-03-23 16:18:49 -0300

    Update login.php

--- account/inc/login.php
+++ account/inc/login.php
@@ -106,6 +106,7 @@ if (isset($_GET['method']) && $_GET['method'] == 'discord') {
                 $_SESSION['phone'] = $users_phone;
                 $_SESSION['auth'] = $users_auth;
                 $_SESSION['loggedin'] = TRUE;
+                echo 'test';
                 header("location: " . $_CONFIG['accounturl']);
             }
         } else {
